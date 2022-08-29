a84b30809ff94c1a21c8e78bf93545ffa62ec220
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2022-08-29 10:22:14 +0800

    refresh

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function($) {
+    "use strict"
+
+    // Navbar on scrolling
+
+    $(window).scroll(function (){
+        if($(this).scrollTop()>200){
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        }else{
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    })
+
+    // Typed  Initiate
+
+    if($('.typed-text-output').length == 1){
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output',{
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+})(jQuery);
